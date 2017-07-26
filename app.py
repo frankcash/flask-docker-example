2579669ce30cf9ddcc61d430bbbb1b3c0f5cc245
Frank Cash <cashc@acm.org>
2017-07-26 09:20:05 -0400

    Returns json

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return 'Flask Dockerized'
+    resp = {
+        "message": "flask is running"
+    }
+    return jsonify(resp)
 
 
 if __name__ == '__main__':
