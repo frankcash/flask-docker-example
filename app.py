a04c8e9920d7f3e64ed0348516dc624eb31d0d20
Frank Cash <cashc@acm.org>
2017-08-24 08:45:54 -0400

    Bokeh example

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from bokeh.plotting import figure
+from bokeh.charts import Histogram
+from bokeh.embed import components
 app = Flask(__name__)
+
 
 @app.route('/')
 def helloWorld():
@@ -7,6 +12,18 @@ def helloWorld():
         "message": "flask is running"
     }
     return jsonify(resp)
+
+
+@app.route("/bokeh")
+def index():
+    df = pd.read_csv("./data/shoes.csv")
+    y = df["sales"]
+    x = df["size"]
+    plot = figure(title="Sales by Size", x_axis_label='size', y_axis_label='sales')
+    plot.line(x, y, legend="Sales", line_width=2)
+    script, div = components(plot)
+    return render_template('graph.html', script=script, div=div)
+
 
 
 if __name__ == '__main__':
