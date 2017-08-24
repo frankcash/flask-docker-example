8f11e447ec49de5274905bfc0d60df9c13a2e0fd
Frank Cash <cashc@acm.org>
2017-08-24 09:29:29 -0400

    Implements proper docker config

--- app.py
+++ app.py
@@ -15,12 +15,22 @@ def helloWorld():
 
 
 @app.route("/bokeh")
-def index():
+def default_bokeh():
     df = pd.read_csv("./data/shoes.csv")
     y = df["sales"]
     x = df["size"]
     plot = figure(title="Sales by Size", x_axis_label='size', y_axis_label='sales')
     plot.line(x, y, legend="Sales", line_width=2)
+    script, div = components(plot)
+    return render_template('graph.html', script=script, div=div)
+
+@app.route("/bokeh/<column>")
+def dynamic(column):
+    df = pd.read_csv("./data/shoes.csv")
+    y = df[column]
+    x = df["size"]
+    plot = figure(title=column + " by Size", x_axis_label='size', y_axis_label=column)
+    plot.line(x, y, legend=column, line_width=2)
     script, div = components(plot)
     return render_template('graph.html', script=script, div=div)
 
